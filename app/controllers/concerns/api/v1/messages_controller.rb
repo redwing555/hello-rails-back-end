bf0ddde129b49a8c2dea7c0dae199d2719730e1f
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-15 14:23:18 +0100

    add route for api controller action

--- app/controllers/concerns/api/v1/messages_controller.rb
+++ app/controllers/concerns/api/v1/messages_controller.rb
@@ -1,4 +1,4 @@
-class MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
     def index
         @message = Message.all.sample(1)
         render json: { :messages => @message}.to_json
