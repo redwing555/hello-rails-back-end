05bd6abc268ab0ab44c4951ce92f9dab5f94710c
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-15 14:18:20 +0100

    run migrations and seed database with sample messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greeting_1 = Message.create(greeting: "Hello folks! ")
+greeting_2 = Message.create(greeting: "Hello dear friends! what a beautiful day to hit the beach! ")
+greeting_3 = Message.create(greeting: "Hello everyone! nice to meet you all! ")
+greeting_4 = Message.create(greeting: "Hi brothers and sisters! what a great day to celebrate! ")
+greeting_5 = Message.create(greeting: "I am glad meeting you folks! what a nice day! ") 
